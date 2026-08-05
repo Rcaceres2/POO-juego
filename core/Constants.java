324bcd75cd53e53fec93e242ac78068ccc07f90d
Rcaceres2 <reynaldocaceres0@gmail.com>
2026-08-04 19:52:59 -0700

    Add files via upload

---
+++ core/Constants.java
@@ -0,0 +1,7 @@
+package core;
+
+public class Constants {
+    public static final int WIDTH = 480;
+    public static final int HEIGHT = 640;
+    public static final double DRAW_INTERVAL = 1000000000.0 / 60.0;
+}
