6990d812e7b85798707e59e967ac6c89d4cbff45
Rcaceres2 <reynaldocaceres0@gmail.com>
2026-08-04 22:45:54 -0400

    Inicializador de componentes

--- Main.java
+++ Main.java
@@ -1,2 +1,30 @@
+import controllers.GameController;
+import controllers.InputController;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import models.WorldModel;
+import views.GamePanel;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Yare Invasion");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+            frame.setResizable(false);
+
+            WorldModel model = new WorldModel();
+            GamePanel panel = new GamePanel(model);
+            InputController inputController = new InputController();
+            GameController controller = new GameController(model, panel, inputController);
+
+            panel.setRestartAction(controller::restartGame);
+
+            frame.add(panel);
+            frame.pack();
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+            controller.startGame();
+        });
+    }
 }
